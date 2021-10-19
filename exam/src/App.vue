c624114786f7e0003a3d5ea5bb99abf30adee1e0
wedrio <ljz1647736549@163.com>
2021-10-19 20:42:58 +0800

    first commit

---
+++ exam/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import './assets/css/icon/iconfont.css'
+import baseurl from './assets/api'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+ul {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: "Microsoft YaHei", "Helvetica", "Tahoma", "Geneva", "Arial", sans-serif;
+  background-color: #eee;
+}
+</style>
